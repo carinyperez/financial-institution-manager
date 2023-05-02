f189f68c3218fee99e89442101f659ddce2b4183
Cariny <perezcariny@gmail.com>
2023-05-02 07:22:32 -0700

    feat: add a homepage and search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Homepage';
+import {FaSearch} from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	  	<h1>Financial Institution Manager</h1>
+		<nav className='header-search'>
+			<input type='text' name='search' value='' placeholder='search'/>
+			<FaSearch/>
+		</nav>
       </header>
+	  <HomePage/>
     </div>
   );
 }
---
+++ src/Homepage.tsx
@@ -0,0 +1,12 @@
+import bankfind from './bankfind.svg'
+import './Homepage.css';
+
+const HomePage = (): JSX.Element => {
+	return (
+		<main>
+			<img src={bankfind} alt='bank find'></img>
+		</main>
+	)
+}
+
+export default HomePage; 
